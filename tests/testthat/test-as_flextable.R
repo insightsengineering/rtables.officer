dbeebe7968d02e8f15b5eb5b007bb64c081d8744
Ezequiel Anokian <lainjoso@hotmail.com>
2026-03-14 15:24:52 +0100

    added unit tests for apply_alignments()

--- tests/testthat/test-as_flextable.R
+++ tests/testthat/test-as_flextable.R
@@ -293,3 +293,68 @@ test_that("tt_to_flextable handles rlistings with multiple header lines", {
     rep(1, ncol(lsting)) # only one cell per column name
   )
 })
+
+test_that("apply_alignments works as expected", {
+
+  test_data_simple <- data.frame(
+    USUBJID = paste0("S", 1:3),
+    ARM = c("A", "A", "B"),
+    AETOXGR = 1:3,
+    AEDECOD = LETTERS[1:3],
+    AESEV = c("MILD", "MOD", "SEVERE")
+  ) |> formatters::var_relabel(
+    USUBJID = "Subject ID",
+    ARM = "Treatment Arm",
+    AETOXGR = "Toxicity \nGrade", # origin of the issue
+    AEDECOD = "Adverse Event"
+  )
+  lsting <- as_listing(
+    df = test_data_simple,
+    key_cols = c("USUBJID"),
+    disp_cols = c("ARM", "AETOXGR", "AEDECOD", "AESEV"),
+    add_trailing_sep = NULL # No separators
+  )
+  out <- tt_to_flextable(lsting)
+
+  alignments_header_before <- out$header$styles$pars$text.align$data
+  alignments_body_before <- out$body$styles$pars$text.align$data
+  alignments_footer_before <- out$footer$styles$pars$text.align$data
+
+  # update alignments in "body"
+  aligns_df <- matrix(data = "left", nrow = nrow(test_data_simple), ncol = ncol(test_data_simple))
+  aligns_df[1, 1] <- "right"
+  aligns_df[3, 3] <- "right"
+  expected_alignments <- aligns_df
+  colnames(expected_alignments) <- colnames(test_data_simple)
+  expect_no_error(res <- apply_alignments(flx = out, aligns_df = aligns_df, part = "body"))
+  alignments_body_after <- res$body$styles$pars$text.align$data
+  expect_equal(expected_alignments, alignments_body_after)
+  alignments_header_after <- res$header$styles$pars$text.align$data
+  alignments_footer_after <- res$footer$styles$pars$text.align$data
+  expect_equal(alignments_header_before, alignments_header_after)
+  expect_equal(alignments_footer_before, alignments_footer_after)
+
+
+  # update alignments in another part, e.g. in "header"
+  aligns_df <- matrix(data = "left", nrow = flextable::nrow_part(out, "header"), ncol = ncol(test_data_simple))
+  aligns_df[1, 3] <- "right"
+  expected_alignments <- aligns_df
+  colnames(expected_alignments) <- colnames(test_data_simple)
+  expect_no_error(res <- apply_alignments(flx = out, aligns_df = aligns_df, part = "header"))
+  alignments_header_after <- res$header$styles$pars$text.align$data
+  expect_equal(expected_alignments, alignments_header_after)
+  alignments_body_after <- res$body$styles$pars$text.align$data
+  alignments_footer_after <- res$footer$styles$pars$text.align$data
+  expect_equal(alignments_body_before, alignments_body_after)
+  expect_equal(alignments_footer_before, alignments_footer_after)
+
+
+  # cases with error
+  expect_error(res <- apply_alignments(flx = out, aligns_df = aligns_df, part = "another part"),
+               "Must be element of set \\{'header','body','footer'\\}, but is 'another part'.")
+
+  aligns_df_2 <- matrix(data = "left", nrow = nrow(test_data_simple) + 1, ncol = ncol(test_data_simple))
+  expect_error(res <- apply_alignments(flx = out, aligns_df = aligns_df_2, part = "body"),
+               "Assertion on 'nrow\\(aligns_df\\) == flextable::nrow_part\\(flx, part\\)' failed: Must be TRUE.")
+
+})
